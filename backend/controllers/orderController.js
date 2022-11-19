46c2ea90d6247f0542c4853b38c716f85f641dcf
nishant1931 <nishoo1902@gmail.com>
2022-11-19 13:10:27 +0530

    remove rzp routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { instance } from "../server.js";
-import crypto from "crypto";
+// import { instance } from "../server.js";
+// import crypto from "crypto";
 
 // POST  PROTECT POST ORDERS
 const addOrderItems = asyncHandler(async (req, res) => {
@@ -51,43 +51,43 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // RAZORPAY POST REQUEST PAYMENT ONLY CREATED - not paid yet
-const payOrder = asyncHandler(async (req, res) => {
-  const options = {
-    amount: Number(req.body.totalPrice * 100), // amount in the smallest currency unit
-    currency: "INR",
-    receipt: "order-receipt",
-  };
-  const order = await instance.orders.create(options);
-  console.log(order);
-  res.status(200).json({ success: "true", order });
-});
+// const payOrder = asyncHandler(async (req, res) => {
+//   const options = {
+//     amount: Number(req.body.totalPrice * 100), // amount in the smallest currency unit
+//     currency: "INR",
+//     receipt: "order-receipt",
+//   };
+//   const order = await instance.orders.create(options);
+//   console.log(order);
+//   res.status(200).json({ success: "true", order });
+// });
 
-const paymentVerification = async (req, res) => {
-  console.log(req.body);
-  // const { razorpay_payment_id, razorpay_signature, razorpay_order_id } =
-  //   req.body;
+// const paymentVerification = async (req, res) => {
+//   console.log(req.body);
+//   const { razorpay_payment_id, razorpay_signature, razorpay_order_id } =
+//     req.body;
 
-  // const body = razorpay_order_id + "|" + razorpay_payment_id;
+//   const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-  // const expectedSignature = crypto
-  //   .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-  //   .update(body.toString())
-  //   .digest("hex");
-  // console.log("sig received ", razorpay_signature);
-  // console.log("sig generated ", expectedSignature);
+//   const expectedSignature = crypto
+//     .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+//     .update(body.toString())
+//     .digest("hex");
+//   console.log("sig received ", razorpay_signature);
+//   console.log("sig generated ", expectedSignature);
 
-  // const isAuthenticPayment = expectedSignature === razorpay_signature;
+//   const isAuthenticPayment = expectedSignature === razorpay_signature;
 
-  // if (isAuthenticPayment) {
-  //   // SAVE IN DATABASE
+//   if (isAuthenticPayment) {
+//     // SAVE IN DATABASE
 
-  //   res.redirect(`/paymentsuccess?reference=${razorpay_payment_id}`);
-  // } else {
-  //   res.status(200).json({
-  //     success: true,
-  //   });
-  // }
-};
+//     res.redirect(`/paymentsuccess?reference=${razorpay_payment_id}`);
+//   } else {
+//     res.status(200).json({
+//       success: true,
+//     });
+//   }
+// };
 
 // Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,8 +4,6 @@ import {
   getAllOrders,
   getMyOrders,
   getOrderById,
-  paymentVerification,
-  payOrder,
   updateOrderToDeliver,
   updateOrderToPaid,
 } from "../controllers/orderController.js";
@@ -18,10 +16,9 @@ router
   .get(protect, admin, getAllOrders)
   .post(protect, addOrderItems);
 router.route("/myorders").get(protect, getMyOrders);
-router.route("/paymentverification").post(paymentVerification);
-router.route("/:id").get(protect, getOrderById).post(payOrder);
+router.route("/paymentverification");
+router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
-
 router.route("/:id/deliver").put(protect, admin, updateOrderToDeliver);
 
 export default router;
